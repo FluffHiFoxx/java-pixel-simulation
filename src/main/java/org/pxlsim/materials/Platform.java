b48e510df8893c863d85e4649243f5e62513c3d4
FluffHiFoxx <hakos2k@gmail.com>
2023-02-21 13:51:38 +0100

    Add: Static material

---
+++ src/main/java/org/pxlsim/materials/Platform.java
@@ -0,0 +1,9 @@
+package org.pxlsim.materials;
+
+import javafx.scene.paint.Color;
+
+public class Platform extends Material {
+    public Platform(int xLimit, int yLimit, int x, int y, Color color) {
+        super(xLimit, yLimit, x, y, color);
+    }
+}
