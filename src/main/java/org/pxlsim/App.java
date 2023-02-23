bfbb850a1d5a744b467279121dbc9a39c8d04210
FluffHiFoxx <hakos2k@gmail.com>
2023-02-23 15:11:32 +0100

    Refactor: materials to collect in a set

--- src/main/java/org/pxlsim/App.java
+++ src/main/java/org/pxlsim/App.java
@@ -14,17 +14,16 @@ import javafx.util.Duration;
 import org.pxlsim.materials.DynamicMaterial;
 import org.pxlsim.materials.Material;
 import org.pxlsim.materials.SandMaterial;
-import org.pxlsim.util.Coordinates;
 
 import java.math.BigDecimal;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.HashSet;
+import java.util.Set;
 
 public class App extends Application {
     final BigDecimal REFRESH_RATE = new BigDecimal("0.02");
     final int WIDTH = 640;
     final int HEIGHT = 360;
-    final Map<Coordinates, Material> MATERIALS = new HashMap<>();
+    final Set<Material> MATERIALS = new HashSet<>();
 
     public static void main(String[] args) {
         launch(args);
@@ -32,7 +31,7 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-        MATERIALS.put(new Coordinates(320, 180), new SandMaterial(WIDTH, HEIGHT, 320, 180, Color.BEIGE, REFRESH_RATE));
+        MATERIALS.add(new SandMaterial(WIDTH, HEIGHT, 320, 180, Color.BEIGE, REFRESH_RATE));
         StackPane root = new StackPane();
         Canvas canvas = new Canvas(WIDTH, HEIGHT);
         GraphicsContext graphics = canvas.getGraphicsContext2D();
@@ -53,7 +52,7 @@ public class App extends Application {
     }
 
     private void handleContent() {
-        for (Material mat : MATERIALS.values()) {
+        for (Material mat : MATERIALS) {
             if (mat instanceof DynamicMaterial) {
                 ((DynamicMaterial) mat).move();
             }
@@ -64,7 +63,7 @@ public class App extends Application {
         graphics.clearRect(0, 0, WIDTH, HEIGHT);
         graphics.setFill(Color.BLACK);
         graphics.fillRect(0, 0, WIDTH, HEIGHT);
-        for (Material mat : MATERIALS.values()) {
+        for (Material mat : MATERIALS) {
             graphics.getPixelWriter().setColor(mat.getX(), mat.getY(), mat.getColor());
         }
     }
